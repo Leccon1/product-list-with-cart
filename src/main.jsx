a6a5adc44db3640696d6b93f7ec3065bd79f9b1f
Leccon1 <dlavania703@gamil.com>
2025-11-04 23:46:32 +0300

    chore: update path in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+
 import App from './App'
-import './sass/main.scss'
+import './style/main.scss'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
