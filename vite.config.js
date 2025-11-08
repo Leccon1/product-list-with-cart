3f579d74c82c26f41a6547cd647b651f5413f244
Leccon1 <dlavania703@gamil.com>
2025-11-08 22:14:32 +0300

    chore: add new path aliases

--- vite.config.js
+++ vite.config.js
@@ -30,6 +30,8 @@ export default defineConfig({
       '@images': path.resolve(__dirname, './src/assets/images'),
       '@icons': path.resolve(__dirname, './src/assets/icons'),
       '@fonts': path.resolve(__dirname, './src/assets/fonts'),
+      '@data': path.resolve(__dirname, './src/data'),
+      '@components': path.resolve(__dirname, './src/components'),
     },
   },
 })
