eb0350ed10f81713242632179bde402b2aac1f8b
Leccon1 <dlavania703@gamil.com>
2025-11-11 00:26:02 +0300

    fix: path to the image

--- src/components/ui/icons/IconRemove.jsx
+++ src/components/ui/icons/IconRemove.jsx
@@ -1,8 +1,8 @@
 // eslint-disable-next-line import/no-unresolved
-import iconremoveSvg from '@icons/icon-remove.svg'
+import iconRemoveSvg from '@icons/icon-remove-item.svg'
 
 const IconRemove = () => {
-  return <img src={iconremoveSvg} alt="Icon remove" />
+  return <img src={iconRemoveSvg} alt="Icon remove" />
 }
 
 export default IconRemove
